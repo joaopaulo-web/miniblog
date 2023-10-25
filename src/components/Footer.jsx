b01d83c10e031ddc27fe85b49254804b8d7d9025
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-10-25 14:30:06 -0300

    Alteração caminho dos ícones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <p>João Paulo Dev &copy; 2023</p>
       <div className={styles.redes_footer}>
        <p>
-          <a href="https://www.linkedin.com/in/joao-paulo-web/"> <img src="src/assets/linkedin.svg" alt="Ícone Linkedin"/> Linkedin</a> 
+          <a href="https://www.linkedin.com/in/joao-paulo-web/"> <img src="/src/assets/linkedin.svg" alt="Ícone Linkedin"/> Linkedin</a> 
         </p>
         <p>
-          <a href="https://github.com/joaopaulo-web"> <img src="src/assets/github.svg" alt="Ícone GitHub"/> GitHub</a>
+          <a href="https://github.com/joaopaulo-web"> <img src="/src/assets/github.svg" alt="Ícone GitHub"/> GitHub</a>
         </p>
       </div>
     </footer>
